e7a048468d8b88d984779adb5c8ef3420633439d
askdfjlas <kaihengd@gmail.com>
2021-01-22 21:49:58 -0500

    Potentially working 404 redirect

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import CpNotes from '../Home/CpNotes';
 
-function Home() {
+function Home({ history }) {
+  if(window.location.href.includes('?')) {
+    const correctPath = window.location.href.split('?')[1].replaceAll('#', '/');
+    history.replace(correctPath);
+  }
+
   return (
     <>
       <CpNotes />
