ec9c580af616f90706bc9287cab06f691650abb8
askdfjlas <kaihengd@gmail.com>
2021-04-22 23:19:57 -0400

    404 attempt 2

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,7 +4,7 @@ import CpNotes from '../Home/CpNotes';
 function Home({ history }) {
   if(window.location.href.includes('?')) {
     const urlSegments = window.location.href.split('?').splice(1);
-    const correctPath = urlSegments.split('#').join('/');
+    const correctPath = urlSegments.join('').split('#').join('/');
     history.replace(correctPath);
   }
 
